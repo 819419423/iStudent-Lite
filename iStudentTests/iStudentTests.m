af4301369072527438b1feeb9dda1b062a415dc9
819419423 <daijing24@gmail.com>
2013-09-04 20:34:52 -0500

    Initial Commit

---
+++ iStudentTests/iStudentTests.m
@@ -0,0 +1,36 @@
+//
+//  iStudentTests.m
+//  iStudentTests
+//
+//  Created by Kobe Dai on 9/4/13.
+//  Copyright (c) 2013 Jing Dai. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iStudentTests : XCTestCase
+
+@end
+
+@implementation iStudentTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
